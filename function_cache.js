5a9245d4bbca0591011d3642aad8fae9cf098413
Jordan Bautista <jordanbau@gmail.com>
2017-06-22 10:04:49 -0700

    problem

---
+++ function_cache.js
@@ -0,0 +1,21 @@
+/*
+
+If you are calculating complex things or execute time-consuming API calls, you sometimes want to cache the results. In this case we want you to create a function wrapper, which takes a function and caches its results depending on the arguments, that were applied to the function.
+ 
+var complexFunction = function(arg1, arg2) {};
+var cachedFunction = cache(complexFunction);
+
+cachedFunction('foo', 'bar'); // complex function should be executed
+cachedFunction('foo', 'bar'); // complex function should not be invoked again, instead the cached result should be returned
+cachedFunction('foo', 'baz'); // should be executed, because the method wasn't invoked before with these arguments
+
+ */
+
+function cache(func) {
+  // do your magic here
+  let cached = {};
+  
+  return () => {
+    if(
+  };
+}
